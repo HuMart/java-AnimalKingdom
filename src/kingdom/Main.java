57aac63354aa8ec57bc00f0b8798f77060d1021f
HuMart <manya01@protonmail.com>
2020-02-20 16:24:10 -0500

    created class mammals and print it out rakoon object

--- src/kingdom/Main.java
+++ src/kingdom/Main.java
@@ -1,9 +1,10 @@
-package kingdom;
+package kingdom;	
 
 public class Main 
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Kingdom");
+		Mammals rakoon = new Mammals("Rakoon", 1758);
+		System.out.println(rakoon);
 	}
 }
--- src/kingdom/Mammals.java
+++ src/kingdom/Mammals.java
@@ -3,4 +3,36 @@ package kingdom;
 public class Mammals extends Animal
 {
 	// constructor
+	public Mammals(String name, int yearDiscovered)
+	{
+		super(name, yearDiscovered);
+	}
+
+	// methods
+
+	@Override
+	public String move()
+	{
+		return "walks";
+	}
+
+	@Override
+	public String breath()
+	{
+		return "breath with lungs";
+	}
+
+	@Override
+	public String reproduce()
+	{
+		return "gives birth to live yung";
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Mammals { "+
+			   "name= '" + name + '\'' +
+			   "yearDiscovered= " + yearDiscovered + '}';
+	}
 }
